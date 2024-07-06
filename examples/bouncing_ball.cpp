db22891b6fc916daf20a00ea80be8b9df0951351
Beverly Xu <xu21beve@gmail.com>
2024-07-05 22:12:21 -0700

    clean and add comments to bouncing ball example

--- examples/bouncing_ball.cpp
+++ examples/bouncing_ball.cpp
@@ -1,96 +1,70 @@
-#include <ompl/base/Planner.h>
-#include <ompl/tools/config/SelfConfig.h>
-#include <ompl/base/GoalTypes.h>
-#include "../HyRRT.h"
-#include "ompl/base/spaces/RealVectorStateSpace.h"
-#include <ompl/control/SimpleSetup.h>
-#include <ompl/base/goals/GoalState.h>
-#include "../CommonMath/Trajectory.hpp"
-#include "../CommonMath/RectPrism.hpp"
-#include "../polyFit.h"
-#include "../src/quartic.cpp"
-#include "../CommonMath/RectPrism.hpp"
-#include "../CommonMath/ConvexObj.hpp"
-#include <list>
 #include "../HyRRT.h"
 
-using namespace CommonMath;
+/** \brief Function computes the Pythagorean distance between two states. */
+double distanceFunc(ompl::base::State *state1, ompl::base::State *state2)
+{
+    double dist = 0;
+    dist = sqrt(pow(state1->as<ompl::base::RealVectorStateSpace::StateType>()->values[0] - state2->as<ompl::base::RealVectorStateSpace::StateType>()->values[0], 2) + pow(state1->as<ompl::base::RealVectorStateSpace::StateType>()->values[1] - state2->as<ompl::base::RealVectorStateSpace::StateType>()->values[1], 2));
+    return fabs(dist);
+}
 
+/** \brief Jump set is whenever the ball is on or below the surface and has a downwards velocity. */
+bool jumpSet(ompl::base::State *state)
+{
+    double velocity = state->as<ompl::base::RealVectorStateSpace::StateType>()->values[1];
+    double pos_cur = state->as<ompl::base::RealVectorStateSpace::StateType>()->values[0];
 
-    double distanceFunc(ompl::base::State *state1, ompl::base::State *state2)
-    {
-        double dist = 0;
-        dist = sqrt(pow(state1->as<ompl::base::RealVectorStateSpace::StateType>()->values[0] - state2->as<ompl::base::RealVectorStateSpace::StateType>()->values[0], 2) + pow(state1->as<ompl::base::RealVectorStateSpace::StateType>()->values[1] - state2->as<ompl::base::RealVectorStateSpace::StateType>()->values[1], 2));
-        return fabs(dist);
-    }
-
-    bool jumpSet(ompl::base::State *state)
-    {
-        double velocity = state->as<ompl::base::RealVectorStateSpace::StateType>()->values[1];
-        double acceleration_cur = state->as<ompl::base::RealVectorStateSpace::StateType>()->values[2];
-        double pos_cur = state->as<ompl::base::RealVectorStateSpace::StateType>()->values[0];
-
-        if (pos_cur <= 0 && velocity <= 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    } // Random selection if in intersection --- 0 if jump, 1 if flow
-
-    bool flowSet(ompl::base::State *state)
-    {
-        return !jumpSet(state);
-    }
-
-    bool unsafeSet(ompl::base::State *state)
-    {
-        std::vector<double> x_cur = {state->as<ompl::base::RealVectorStateSpace::StateType>()->values[0], state->as<ompl::base::RealVectorStateSpace::StateType>()->values[1], state->as<ompl::base::RealVectorStateSpace::StateType>()->values[2], state->as<ompl::base::RealVectorStateSpace::StateType>()->values[3]};
-        if (x_cur[0] > 10)
-            return true;
+    if (pos_cur <= 0 && velocity <= 0)
+        return true;
+    else
         return false;
-    }
+}
 
-    ompl::base::State *flowPropagation(std::vector<double> inputs, ompl::base::State *x_cur, double tFlow, ompl::base::State *new_state)
-    {
-        double velocity = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[1];
-        double acceleration_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[2];
-        double pos_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[0];
-        double tFlow_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[3];
-        double tJump_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[4];
+/** \brief Flow set is whenever the ball is above the surface or has an upwards velocity. */
+bool flowSet(ompl::base::State *state)
+{
+    return !jumpSet(state);
+}
 
-        double v = velocity + (acceleration_cur)*tFlow;                               // v = v0 + at
-        double x = pos_cur + velocity * tFlow + (acceleration_cur) * pow(tFlow, 2) / 2; // x = v0 * t + 1/2(at^2)
-        double tFlow_new = tFlow_cur + tFlow;
+/** \brief Unsafe set is whenever the ball is above 10 units from the ground, to reduce time spent planning. */
+bool unsafeSet(ompl::base::State *state)
+{
+    if (state->as<ompl::base::RealVectorStateSpace::StateType>()->values[0] > 10)
+        return true;
+    return false;
+}
 
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[0] = x;
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[1] = v;
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[2] = acceleration_cur; // Flow should not be increasing the acceleration, TODO: schange this later for clarity (make starting acceleration -9.81)
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[3] = tFlow_cur;
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[4] = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[4];  // No change in jump time
-        return new_state;
-    }
+/** \brief Simulates the dynamics of the ball when in flow regime, with no nonnegligble forces other than gravity. */
+ompl::base::State *continuousSimulator(std::vector<double> inputs, ompl::base::State *x_cur, double tFlow, ompl::base::State *new_state)
+{
+    double velocity = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[1];
+    double acceleration_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[2];
+    double pos_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[0];
 
-    ompl::base::State *jumpPropagation(ompl::base::State *x_cur, std::vector<double> u, ompl::base::State *new_state)
-    {
-        double velocity = -0.8 * x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[1];
-        double acceleration = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[2];
-        double pos_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[0];
-        double tFlow_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[3];
-        double tJump_cur = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[4];
+    double v = velocity + (acceleration_cur) * tFlow;                               // v = v0 + at
+    double x = pos_cur + velocity * tFlow + (acceleration_cur) * pow(tFlow, 2) / 2; // x = v0 * t + 1/2(at^2)
 
-        double v = velocity; // v = v0 + at 
-        double x = pos_cur;  // x = v0 * t + 1/2(at^2)
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[0] = x;
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[1] = v;
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[2] = acceleration_cur; 
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[3] = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[3];   // Dynamics simulation functions does not touch flow time or jumps
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[4] = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[4];
+    return new_state;
+}
 
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[0] = x;
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[1] = v - u[0];
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[2] = acceleration;
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[3] = tFlow_cur; // No change in flow time
-        new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[4] = tJump_cur;  // No change in jump time
-        return new_state;
-    }
+/** \brief Simulates the dynamics of the ball when in jump regime, with input from the surface. */
+ompl::base::State *discreteSimulator(ompl::base::State *x_cur, std::vector<double> u, ompl::base::State *new_state)
+{
+    double velocity = -0.8 * x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[1];
+
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[0] = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[0];
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[1] = velocity - u[0];
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[2] = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[2];
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[3] = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[3];   // Dynamics simulation functions does not touch flow time or jumps
+    new_state->as<ompl::base::RealVectorStateSpace::StateType>()->values[4] = x_cur->as<ompl::base::RealVectorStateSpace::StateType>()->values[4];
+    return new_state;
+}
+
 
 int main()
 {
@@ -136,8 +110,8 @@ int main()
     cHyRRT.setProblemDefinition(pdef);
     cHyRRT.setup();
 
-    cHyRRT.setContinuousSimulator(flowPropagation);
-    cHyRRT.setDiscreteSimulator(jumpPropagation);
+    cHyRRT.setContinuousSimulator(continuousSimulator);
+    cHyRRT.setDiscreteSimulator(discreteSimulator);
     cHyRRT.setDistanceFunction(distanceFunc);
     cHyRRT.setFlowSet(flowSet);
     cHyRRT.setJumpSet(jumpSet);
@@ -152,7 +126,7 @@ int main()
     ompl::base::PlannerStatus solved = cHyRRT.solve(ompl::base::timedPlannerTerminationCondition(10000));
     cout << "solution status: " << solved << endl;
 
-    // // How to access the solution path as a C++ vector
+    // // How to access the solution path as a vector
     // std::vector<ompl::geometric::HyRRT::Motion *> trajectory = cHyRRT.getTrajectoryMatrix();
     // for(auto &m : trajectory) {
     //     // Use m->as<ompl::base::RealVectorStateSpace::StateType>()->values[** desired index **]
